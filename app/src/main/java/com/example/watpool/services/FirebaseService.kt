1c6594ee5d06c529a28b87da337add476d0ef164
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2024-06-15 23:56:18 -0400

    Firebase Setup (#8)
    * firebase setup
    
    * timelog update

---
+++ app/src/main/java/com/example/watpool/services/FirebaseService.kt
@@ -0,0 +1,10 @@
+package com.example.watpool.services
+
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+
+
+// TODO: Add helper functions here to interact with the DB
+class FirebaseService {
+    private val database = Firebase.database
+}
